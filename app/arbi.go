e720682136820307ced4b78b85c3c9ad06208d70
Olaf Szymanski <olaf.szym123@gmail.com>
2022-06-16 13:42:14 +0200

    Clean up logger

--- app/arbi.go
+++ app/arbi.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-	"time"
-
 	"github.com/olafszymanski/arbi/app/config"
 	"github.com/olafszymanski/arbi/app/internal/exchange"
 	"github.com/olafszymanski/arbi/app/internal/postgres"
-	"github.com/rs/zerolog"
+	"github.com/olafszymanski/arbi/app/pkg/logger"
 )
 
 func main() {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	output.FormatLevel = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf("|  %-6s|", i))
-	}
-	output.FormatMessage = func(i interface{}) string {
-		return fmt.Sprintf("%s      ", i)
-	}
-	output.FormatFieldName = func(i interface{}) string {
-		return fmt.Sprintf("%s: ", i)
-	}
-	output.FormatFieldValue = func(i interface{}) string {
-		return fmt.Sprintf("%s", i)
-	}
-	l := zerolog.New(output).With().Timestamp().Logger()
-
+	l := logger.NewLogger()
 	cfg := config.NewConfig(&l)
 	s := postgres.NewStore(&l, cfg)
 	binance := exchange.NewBinance(&l, cfg, s, map[string][]string{
---
+++ app/pkg/logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+func NewLogger() zerolog.Logger {
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("|  %-6s|", i))
+	}
+	output.FormatMessage = func(i interface{}) string {
+		return fmt.Sprintf("%s      ", i)
+	}
+	output.FormatFieldName = func(i interface{}) string {
+		return fmt.Sprintf("%s: ", i)
+	}
+	output.FormatFieldValue = func(i interface{}) string {
+		return fmt.Sprintf("%s", i)
+	}
+	return zerolog.New(output).With().Timestamp().Logger()
+}
