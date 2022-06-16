96987dd2ce31f92b6b2ea07b6e92a976386815b9
Olaf Szymanski <olaf.szym123@gmail.com>
2022-06-16 17:59:06 +0200

    Remove unnecessary code

--- app/internal/postgres/binance.go
+++ app/internal/postgres/binance.go
@@ -1,9 +1,6 @@
 package postgres
 
 import (
-	"log"
-	"time"
-
 	"github.com/jmoiron/sqlx"
 )
 
@@ -16,8 +13,6 @@ func NewBinanceStore(db *sqlx.DB) *BinanceStore {
 }
 
 func (b *BinanceStore) CreateRecord(r Record) error {
-	s := time.Now()
 	_, err := b.db.DB.Exec("INSERT INTO records (lowSymbol, lowPrice, highSymbol, highPrice, value, timestamp) VALUES ($1, $2, $3, $4, $5, $6)", r.Low.Symbol, r.Low.Price, r.High.Symbol, r.High.Price, r.Value, r.Timestamp)
-	log.Println("Elapsed: ", time.Since(s))
 	return err
 }
