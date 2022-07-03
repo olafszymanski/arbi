24122d1756a0fa61053e4d7b3c53c2e700ed91d7
Olaf Szymanski <olaf.szym123@gmail.com>
2022-07-04 00:18:14 +0200

    Add validator description

--- internal/binance/validator.go
+++ internal/binance/validator.go
@@ -7,6 +7,7 @@ func NewValidator() *Validator {
 	return &Validator{}
 }
 
+// Validates returned JSON files, currently checks if API was able to correctly download bid and ask data along with permissions.
 func (v *Validator) Validate(symbol jsonSymbol, book jsonOrderBook) bool {
 	if symbol.Symbol == book.Symbol && (book.Bid != "0.00000000" && book.Ask != "0.00000000") {
 		for _, p := range symbol.Permissions {
