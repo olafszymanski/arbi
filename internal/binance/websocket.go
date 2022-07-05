5fcca6fb90b645aca1f28ac8118d72385e133551
Olaf Szymanski <olaf.szym123@gmail.com>
2022-07-05 12:55:18 +0200

    Add websocket and updater

---
+++ internal/binance/websocket.go
@@ -0,0 +1,61 @@
+package binance
+
+import (
+	"errors"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type jsonOrderBookTicker struct {
+	Symbol      string `json:"s"`
+	Bid         string `json:"b"`
+	BidQuantity string `json:"B"`
+	Ask         string `json:"a"`
+	AskQuantity string `json:"A"`
+}
+
+type Websocket struct {
+	connection *websocket.Conn
+	factory    *URLFactory
+}
+
+func NewWebsocket(factory *URLFactory) (*Websocket, error) {
+	c, _, err := websocket.DefaultDialer.Dial(factory.OrderBookTickers(), nil)
+	if err != nil {
+		return nil, err
+	}
+	c.SetPingHandler(func(appData string) error {
+		return c.WriteControl(websocket.PongMessage, []byte(nil), time.Now().Add(5*time.Second))
+	})
+	return &Websocket{c, factory}, nil
+}
+
+func (w *Websocket) Read() (*jsonOrderBookTicker, error) {
+	var o jsonOrderBookTicker
+	if err := w.connection.ReadJSON(&o); err != nil {
+		if errors.Is(err, syscall.ECONNRESET) {
+			if err := w.reconnect(); err != nil {
+				return nil, err
+			}
+			return w.Read()
+		} else {
+			return nil, err
+		}
+	}
+	return &o, nil
+}
+
+func (w *Websocket) Close() {
+	w.connection.Close()
+}
+
+func (w *Websocket) reconnect() error {
+	c, _, err := websocket.DefaultDialer.Dial(w.factory.OrderBookTickers(), nil)
+	if err != nil {
+		return err
+	}
+	w.connection = c
+	return nil
+}
