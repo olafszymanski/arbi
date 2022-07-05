4895ce1b961422363a03edcb71d8085866f7b159
Olaf Szymanski <olaf.szym123@gmail.com>
2022-07-05 15:44:07 +0200

    Engine implementation

--- arbi.go
+++ arbi.go
@@ -1,64 +1,16 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"time"
-
 	"github.com/olafszymanski/arbi/config"
-	"github.com/olafszymanski/arbi/internal/broker/binance"
-	"github.com/olafszymanski/arbi/internal/database"
-	log "github.com/sirupsen/logrus"
+	"github.com/olafszymanski/arbi/internal/binance"
 )
 
 func main() {
-	log.SetReportCaller(true)
-	log.SetFormatter(&log.JSONFormatter{})
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	done := make(chan struct{})
-
 	cfg := config.NewConfig()
-	s := database.NewStore(context.Background(), cfg)
-	defer s.Disconnect()
+	// binance.NewEngine(cfg, []string{"USDT", "DAI", "BTC", "ETH", "BNB"})
+	e := binance.NewEngine(cfg, []string{"USDT", "DAI", "BTC", "ETH", "BNB"})
+	e.Run()
 
-	binance := binance.New(cfg, s, map[string][]string{
-		"BTC": {
-			"USDT",
-			"USDC",
-			"TUSD",
-			"DAI",
-			"BUSD",
-		},
-		"ETH": {
-			"USDT",
-			"USDC",
-			"TUSD",
-			"DAI",
-			"BUSD",
-		},
-		"BNB": {
-			"USDT",
-			"USDC",
-			"TUSD",
-			"DAI",
-			"BUSD",
-		},
-	})
-	binance.Subscribe(context.Background(), done)
-
-	for {
-		select {
-		case <-done:
-			return
-		case <-interrupt:
-			select {
-			case <-done:
-			case <-time.After(time.Microsecond):
-			}
-			return
-		}
-	}
+	// log.SetReportCaller(true)
+	// log.SetFormatter(&log.JSONFormatter{})
 }
