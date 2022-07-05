4e8c4ae693660f281c5f8e66e763d1a5e8e9c0b2
Olaf Szymanski <olaf.szym123@gmail.com>
2022-07-05 12:28:47 +0200

    Change parameter name

--- internal/binance/validator.go
+++ internal/binance/validator.go
@@ -8,8 +8,8 @@ func NewValidator() *Validator {
 }
 
 // Validates returned JSON files, currently checks if API was able to correctly download bid and ask data along with permissions.
-func (v *Validator) Validate(symbol jsonSymbol, book jsonOrderBook) bool {
-	if symbol.Symbol == book.Symbol && (book.Bid != "0.00000000" && book.Ask != "0.00000000") {
+func (v *Validator) Validate(symbol jsonSymbol, orderBook jsonOrderBook) bool {
+	if symbol.Symbol == orderBook.Symbol && (orderBook.Bid != "0.00000000" && orderBook.Ask != "0.00000000") {
 		for _, p := range symbol.Permissions {
 			if p == "SPOT" {
 				return true
