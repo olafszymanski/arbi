39e073f87d651644874d0963c49d80baf755326d
Olaf Szymanski <olaf.szym123@gmail.com>
2022-07-08 12:44:00 +0200

    Fix

--- arbi.go
+++ arbi.go
@@ -3,14 +3,17 @@ package main
 import (
 	"github.com/olafszymanski/arbi/config"
 	"github.com/olafszymanski/arbi/internal/binance"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	cfg := config.NewConfig()
-	// binance.NewEngine(cfg, []string{"USDT", "DAI", "BTC", "ETH", "BNB"})
-	e := binance.NewEngine(cfg, []string{"USDT", "DAI", "BTC", "ETH", "BNB"})
-	e.Run()
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.JSONFormatter{})
 
-	// log.SetReportCaller(true)
-	// log.SetFormatter(&log.JSONFormatter{})
+	cfg := config.NewConfig()
+	e, err := binance.NewEngine(cfg, []string{"USDT", "BTC", "ETH", "DAI", "BUSD", "BNB"})
+	if err != nil {
+		log.WithError(err).Panic()
+	}
+	e.Run()
 }
